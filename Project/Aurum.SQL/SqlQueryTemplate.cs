18829684acc802eefb3f5034c764d32c185e5d00
404htm <github@404htm.com>
2016-05-02 23:43:44 -0400

    Added SQL Query Template - Closes #6

---
+++ Project/Aurum.SQL/SqlQueryTemplate.cs
@@ -0,0 +1,24 @@
+﻿using Aurum.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.SQL
+{
+	[DataContract]
+	public class SqlQueryTemplate : StoreableBase<SqlQueryTemplate>
+	{
+		[DataMember] public string Name { get; set; }
+		[DataMember] public string Description { get; set; }
+		[DataMember] public List<string> RequiredColumns { get; set; }
+		[DataMember] public bool IsDestructive { get; set; }
+		[DataMember] public string QueryText { get; set; }
+
+		//TODO: SubQuery Settings
+		//TODO: Output Types
+
+	}
+}
