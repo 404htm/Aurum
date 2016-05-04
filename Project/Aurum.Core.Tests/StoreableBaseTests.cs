fcc09ecbe969eba0eae37731d604a2a69db292d4
404htm <github@404htm.com>
2016-05-03 22:06:43 -0400

    Converted StoreableBase to JSON for reasons...  Added a core test project and some basic tests. Closes #6

---
+++ Project/Aurum.Core.Tests/StoreableBaseTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace Aurum.Core.Tests
+{
+	[TestClass]
+	public class StoreableBaseTests
+	{
+		[DataContract(IsReference = false)]
+		private class TestObjA : StoreableBase<TestObjA>
+		{
+			[DataMember] public string Name { get; set; }
+			[DataMember] public int Test { get; set; }
+			[DataMember] public DateTime date { get; set; }
+		}
+
+		[TestMethod]
+		public void SaveAndReload()
+		{
+			var obj = new TestObjA { Name = "TEST", Test = 5, date = DateTime.Now };
+			var tmp = Path.GetTempFileName();
+
+			obj.Save(tmp);
+			var copy = TestObjA.Load(tmp);
+
+			Assert.IsNotNull(copy);
+			Assert.AreEqual(obj.Name, copy.Name);
+			Assert.AreEqual(obj.Test, copy.Test);
+			Assert.AreEqual(0, obj.date.Subtract(copy.date).Milliseconds,  "Serialized and deserialized dates do not match");
+
+			File.Delete(tmp);
+		}
+
+		
+	}
+}
--- Project/Aurum.Core/StoreableBase.cs
+++ Project/Aurum.Core/StoreableBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -10,40 +11,30 @@ using System.Xml.Linq;
 
 namespace Aurum.Core
 {
-	[DataContract(Namespace = "", IsReference = true)]
+	[DataContract(Namespace = "", IsReference = false)]
 	public abstract class StoreableBase<T> where T : StoreableBase<T>
 	{
 		public void Save(string filename)
 		{
-			var ser = new DataContractSerializer(typeof(T));
-			var xml = new XmlWriterSettings { Indent = true, NewLineHandling = NewLineHandling.Replace, NewLineChars = Environment.NewLine };
-			using (var writer = XmlWriter.Create(filename)) ser.WriteObject(writer, (T)this);
+			var ser = new DataContractJsonSerializer(typeof(T));
+			using (FileStream stream = File.Create(filename))
+			{
+				ser.WriteObject(stream, this);
+			}
 		}
 		public static T Load(string filename)
 		{
-			var ser = new DataContractSerializer(typeof(T));
+			var ser = new DataContractJsonSerializer(typeof(T));
 			T result;
-			using (var reader = XmlReader.Create(filename)) result = (T)ser.ReadObject(reader);
+			using (FileStream stream = File.OpenRead(filename))
+			{
+				result = (T)ser.ReadObject(stream);
+			}
+
 			result.OnDeserialization();
 			return result;
 		}
-		public static T LoadFromXML(string xml)
-		{
-			var ser = new DataContractSerializer(typeof(T));
-			T result;
-			using (var reader = XmlReader.Create(new StringReader(xml))) result = (T)ser.ReadObject(reader);
-			result.OnDeserialization();
-			return result;
-		}
-		public XElement ToXElement()
-		{
-			var ser = new DataContractSerializer(typeof(T));
-			var xml = new XmlWriterSettings { Indent = true, NewLineHandling = NewLineHandling.Replace, NewLineChars = Environment.NewLine };
-			var ms = new MemoryStream();
-			using (var writer = XmlWriter.Create(ms)) ser.WriteObject(writer, (T)this);
-			ms.Position = 0;
-			return XElement.Load(ms);
-		}
+		
 		protected virtual void OnDeserialization()
 		{
 		}
