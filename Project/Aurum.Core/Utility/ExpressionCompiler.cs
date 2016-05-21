9e74800216284bbe764c5c8b74a38a0e35bacd78
404htm <github@404htm.com>
2016-05-20 22:53:07 -0400

    #14 - Templated out a query parser - Expression Tree is currently hard-coded

---
+++ Project/Aurum.Core/Utility/ExpressionCompiler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Core.Utility
+{
+	public class ExpressionCompiler<T>
+	{
+		List<ParameterExpression> _parameters = new List<ParameterExpression>();
+		List<Expression> _variables = new List<Expression>();
+
+		public void SetParameter<P>(string name)
+		{
+			_parameters.Add(Expression.Parameter(typeof(P), name));
+		}
+
+
+
+		//	public void AddCapture<T>(string name, T obj)
+		//	{
+		//		var @const = Expression.Constant(obj, typeof(T));
+		//		var @var = Expression.Variable(typeof(T), name);
+		//		var assignment = Expression.Assign(@var, @const);
+		//		_variables.Add(@var);
+		//		_boilerplate.Add(assignment);
+		//	}
+		//	
+
+		public T CompileStatement(string statement)
+		{
+			var block = parse(statement);
+			var result = Expression.Lambda<T>(block, _parameters.ToArray()).Compile();
+			return result;
+		}
+
+		private BlockExpression parse(string statement)
+		{
+			var block = Expression.Block(Expression.Equal(Expression.Constant(5), Expression.Constant(5)));
+			return block;
+		}
+	}
+}
