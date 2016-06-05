8b535dccf8a75ed3b27eea21e90bbdeb9f934177
404htm <github@404htm.com>
2016-06-04 23:58:42 -0400

    #24 - Working on type mappings for SqlDbType

---
+++ Project/Aurum.SQL/SqlTypeMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DbTypeLookup = System.Collections.Generic.Dictionary<System.Data.SqlDbType, System.Type>;
+
+namespace Aurum.SQL
+{
+	public class SqlTypeMap
+	{
+		//Reference: https://msdn.microsoft.com/en-us/library/system.data.sqldbtype(v=vs.110).aspx
+
+		static readonly DbTypeLookup _sqlDbTypeLookup;
+
+		static SqlTypeMap()
+		{
+			_sqlDbTypeLookup = BuildDbTypeLookup();
+		}
+
+		static DbTypeLookup BuildDbTypeLookup()
+		{
+			return new Dictionary<SqlDbType, Type>
+			{
+				{SqlDbType.BigInt,          typeof(long) },
+				{SqlDbType.Binary,          typeof(byte[]) },
+				{SqlDbType.Bit,             typeof(bool) },
+				{SqlDbType.Char,            typeof(string) },
+				{SqlDbType.Date,            typeof(DateTime) },
+				{SqlDbType.DateTime,        typeof(DateTime) },
+				{SqlDbType.DateTime2,       typeof(DateTime) },
+				{SqlDbType.DateTimeOffset,  typeof(TimeSpan) }, //TODO: Validate this decision
+				{SqlDbType.Decimal,         typeof(decimal) },
+				{SqlDbType.Float,           typeof(float) },
+				{SqlDbType.Image,           typeof(byte[]) },
+				{SqlDbType.Int,             typeof(int) },
+				{SqlDbType.Money,           typeof(decimal) },
+				{SqlDbType.NChar,           typeof(string) },
+				{SqlDbType.NText,           typeof(string)},
+				{SqlDbType.NVarChar,        typeof(string)}
+			};
+		}
+
+	}
+}
