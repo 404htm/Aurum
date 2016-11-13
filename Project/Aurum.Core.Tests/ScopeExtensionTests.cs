1bc18486af414bd97cb910590b69cbe41e77c0d9
404htm <github@404htm.com>
2016-11-13 13:38:50 -0500

    #34: Added extension method to convert scope objects to ExpandoObjects (Supported by CSharpScripting as globals)

---
+++ Project/Aurum.Core.Tests/ScopeExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using Aurum.Core.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace Aurum.Core.Tests
+{
+    [TestClass]
+    public class ScopeExtensionTests
+    {
+        [TestMethod]
+        public void ScopeExtension_ToExpando()
+        {
+            var scope = new Mock<IScope>();
+            scope.Setup(s => s.Get<object>("ItemA")).Returns("A");
+            scope.Setup(s => s.Get<object>("ItemB")).Returns(5);
+            scope.Setup(s => s.Get<object>("ItemC")).Returns('C');
+            scope.Setup(s => s.Get<object>("ItemD")).Returns(null);
+            scope.Setup(s => s.Keys).Returns(new List<string> { "ItemA", "ItemB", "ItemC", "ItemD" });
+
+
+            var result = scope.Object.ToExpando() as dynamic;
+            Assert.AreEqual(result.ItemA, "A");
+            Assert.AreEqual(result.ItemB, 5);
+            Assert.AreEqual(result.ItemC, 'C');
+            Assert.IsNull(result.ItemD);
+        }
+    }
+}
---
+++ Project/Aurum.Core/Extensions/ScopeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp;
+
+namespace Aurum.Core.Extensions
+{
+    public static class ScopeExtensions
+    {
+        /// <summary  an IScope object to an expando object for used by other components </summary>
+        public static ExpandoObject ToExpando(this IScope scope)
+        {
+            var obj = new ExpandoObject();
+            var lookup = obj as IDictionary<string, object>;
+
+            foreach(var key in scope.Keys) lookup[key] = scope.Get<object>(key);
+
+            return obj;
+        }
+    }
+}
