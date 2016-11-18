2747957eadca2b5d8c05904d4edd3e62009e8e44
404htm <github@404htm.com>
2016-11-17 23:27:10 -0500

    No Ticket: Added client project for templates

---
+++ Project/Aurum.Integration.Tests/Templates/Mark2.au.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Integration.Tests.Templates
+{
+    public class Mark2
+    {
+        //:#using System;
+        //:#using System.Collections.Generic;
+        
+    }
+}
