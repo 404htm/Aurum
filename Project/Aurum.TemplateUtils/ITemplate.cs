745b59beaf6f8993ee46eea4c31fec5282991111
404htm <github@404htm.com>
2016-11-18 23:38:35 -0500

    #37: Added starter interface for alternative templating engine

---
+++ Project/Aurum.TemplateUtils/ITemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aurum.TemplateUtils
+{
+    public interface ITemplate<T>
+    {
+        string Source { get; }
+        string FileName { get; }
+        void Body(T data);
+    }
+}
