fe6b2dd25f42335b210d8779c1c26acadb82baa9
404htm <github@404htm.com>
2016-11-19 23:48:46 -0500

    #38: Implemented template rewriter

---
+++ Project/Aurum.Gen.Tests/TemplateRewriterTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Moq;
+using Aurum.Gen.Nodes;
+using Aurum.Core.Parser;
+using System.Collections.Generic;
+using Aurum.Core;
+
+
+namespace Aurum.Gen.Tests
+{
+    [TestClass]
+    public class TemplateRewriterTests
+    {
+        [TestMethod]
+        public void TemplateRewriter_EnsurePlainTextIsntAltered()
+        {
+            Func<string, string> substitution = (line) => "FAIL";
+            var input = new List<string> { "LINE 1", "LINE 2", "LINE 3" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input);
+
+            Assert.AreEqual(3, result.Count());
+            Assert.IsTrue(input.SequenceEqual(result), "Input sequence should be unaltered");
+        }
+
+        [TestMethod]
+        public void TemplateRewriter_EnsureCommentTextIsntAltered()
+        {
+            Func<string, string> substitution = (line) => "FAIL";
+            var input = new List<string> { "LINE 1", "//LINE 2", "//LINE 3" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input);
+
+            Assert.AreEqual(3, result.Count());
+            Assert.IsTrue(input.SequenceEqual(result), "Input sequence should be unaltered");
+        }
+
+
+        [TestMethod]
+        public void TemplateRewriter_EnsureMetaLinesReplaced()
+        {
+            Func<string, string> substitution = (line) => $"-{line}-";
+            var input = new List<string> { "A", ":B", "C" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input).ToList();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("A", result[0]);
+            Assert.AreEqual("-B-", result[1]);
+            Assert.AreEqual("C", result[2]);
+        }
+
+        [TestMethod]
+        public void TemplateRewriter_EnsureCommentMetaLinesReplaced()
+        {
+            Func<string, string> substitution = (line) => $"-{line}-";
+            var input = new List<string> { "A", "//:B", "C" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input).ToList();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("A", result[0]);
+            Assert.AreEqual("-B-", result[1]);
+            Assert.AreEqual("C", result[2]);
+        }
+
+        [TestMethod]
+        public void TemplateRewriter_EnsureCommentMetaAWorksWithLeadingWhitespace()
+        {
+            Func<string, string> substitution = (line) => $"-{line}-";
+            var input = new List<string> { "A", "    //:B", "C" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input).ToList();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("A", result[0]);
+            Assert.AreEqual("-B-", result[1]);
+            Assert.AreEqual("C", result[2]);
+        }
+
+        [TestMethod]
+        public void TemplateRewriter_EnsurePreservesLeadingWhitespace()
+        {
+            Func<string, string> substitution = (line) => $"-{line}-";
+            var input = new List<string> { "A", ":    B", "C" };
+
+            var underTest = new TemplateRewriter(":", "`", substitution);
+            var result = underTest.Rewrite(input).ToList();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("A", result[0]);
+            Assert.AreEqual("-    B-", result[1]);
+            Assert.AreEqual("C", result[2]);
+        }
+
+
+    }
+}
--- Project/Aurum.Gen/TemplateRewriter.cs
+++ Project/Aurum.Gen/TemplateRewriter.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Aurum.Gen
 {
+    /// <summary>
+    /// This class does an intermediary tranform on Aurum templates...
+    /// Instead of explicitly parsing the template it substitutes the meta sections with code that writes to an emitter
+    /// That code can then run using the c# parser and the output can be used to build the resulting code
+    /// </summary>
     public class TemplateRewriter
     {
+        readonly Func<string, string> _substitution;
+        readonly Regex _lineFinder;
+
+
+        public TemplateRewriter(string metaLineSymbol, string metaEscapeSymbol, Func<string, string> substitution)
+        {
+            var line = Regex.Escape(metaLineSymbol);
+            var esc = Regex.Escape(metaEscapeSymbol);
+
+            _lineFinder = new Regex($@"(?<=^\s*({line}|//{line}))(.*)$");
+            _substitution = substitution;
+        }
+
+        public IEnumerable<string> Rewrite(IEnumerable<string> template)
+        {
+            foreach (var line in template)
+            {
+                var result = _lineFinder.Match(line);
+                if (result.Success) yield return _substitution(result.Value);
+                else yield return line;
+            }
+        }
+
+
+
     }
 }
