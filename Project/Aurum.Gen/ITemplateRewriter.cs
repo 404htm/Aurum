4ffa6a8a980b4bc9a590b719059fb7b70598d088
404htm <github@404htm.com>
2016-11-22 20:15:25 -0500

    #40: Initial layout for new template materializer

---
+++ Project/Aurum.Gen/ITemplateRewriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Aurum.Gen
+{
+    public interface ITemplateRewriter
+    {
+        IEnumerable<string> Rewrite(IEnumerable<string> template);
+    }
+}
---
+++ Project/Aurum.Gen/TemplateMaterializer.cs
@@ -0,0 +1,27 @@
+﻿using Aurum.TemplateUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Gen
+{
+    public class TemplateMaterializer<T>
+    {
+        public TemplateMaterializer(ITemplateRewriter rewriter)
+        {
+
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITemplate<T> Compile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
