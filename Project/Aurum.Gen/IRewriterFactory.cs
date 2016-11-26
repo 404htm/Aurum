a1c89ccf719089c1862c074145fc24c7afb6c4f8
404htm <github@404htm.com>
2016-11-25 19:05:53 -0500

    #40: Created a factory for the template rewriter (Needed for the materializer)

---
+++ Project/Aurum.Gen/IRewriterFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Gen
+{
+    public interface IRewriterFactory
+    {
+        ITemplateRewriter Create(Func<string, string> metaReplace, Func<string, string> inlineReplace);
+    }
+}
---
+++ Project/Aurum.Gen/RewriterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Gen
+{
+    public class RewriterFactory : IRewriterFactory
+    {
+        string _metaSymbol;
+        string _inlineSymbolL;
+        string _inlineSymbolR;
+
+        public RewriterFactory(string metaSymbol, string inlineSymbolL, string inlineSymbolR)
+        {
+            _metaSymbol = metaSymbol;
+            _inlineSymbolL = inlineSymbolL;
+            _inlineSymbolR = inlineSymbolR;
+        }
+
+        public ITemplateRewriter Create(Func<string, string> metaReplace, Func<string, string> inlineReplace)
+        {
+            return new TemplateRewriter(_metaSymbol, _inlineSymbolL, _inlineSymbolR, metaReplace, inlineReplace);
+        }
+    }
+}
