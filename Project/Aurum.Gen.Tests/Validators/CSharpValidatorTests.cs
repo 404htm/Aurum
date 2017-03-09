26900af017d34a97d6d6f93cc33622a7a85664a7
404htm <github@404htm.com>
2017-03-08 23:18:44 -0500

    Added code parser and tests, Attempting to resolve  sudden issue with System.Collections.Immutable

---
+++ Project/Aurum.Gen.Tests/Validators/CSharpValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Aurum.Gen.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Gen.Tests.Validators
+{
+    [TestClass]
+    public class CSharpValidatorTests
+    {
+        [TestMethod]
+        public void Gen_VerifyBasicClassDeclarationPasses()
+        {
+            var code = @"
+                using custom.namespace;
+
+                namespace Code.Generated;
+                public class GeneratedCode
+                {
+                    public bool GeneratedMethod()
+                    {
+                        return true;
+                    }
+                };
+            ";
+
+            var underTest = new CSharpValidator();
+            var result = underTest.Parse(code);
+        }
+    }
+}
---
+++ Project/Aurum.Gen/ValidatingCodeEmitter.cs
@@ -0,0 +1,25 @@
+﻿using Aurum.TemplateUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Aurum.Gen.Validators;
+
+namespace Aurum.Gen
+{
+    public class ValidatingCodeEmitter : ICodeEmitter
+    {
+        ICodeValidator _validator;
+        public ValidatingCodeEmitter(ICodeValidator validator)
+        {
+
+        }
+
+        public void WriteLine(string text, [CallerLineNumber] int lineNumber = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
