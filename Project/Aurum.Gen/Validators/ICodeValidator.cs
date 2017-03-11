4a0999cfb5ec8d6052e960009d19911cb16e10ec
404htm <github@404htm.com>
2017-03-11 16:22:12 -0500

    I hate everything.

---
+++ Project/Aurum.Gen/Validators/ICodeValidator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Aurum.Gen.Validators
+{
+    public interface ICodeValidator
+    {
+        List<ValidationResult> Parse(string code);
+
+
+    }
+}
---
+++ Project/Aurum.Gen/Validators/ValidationResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurum.Gen.Validators
+{
+    public class ValidationResult
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public int Start { get; set; }
+        public int Length { get; set; }
+        public int Line { get; set; }
+    }
+}
